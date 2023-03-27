5c429e04d383b0cd431f5f04c69e8288e5cc740a
Ayush <krayushkumar01@gmail.com>
2023-03-28 02:00:34 +0530

    Initial Commit

---
+++ Math Questions/fibonacciseries.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x;
+    cout<<"Enter the number for its series: ";
+    cin>>x;
+    int a=0;
+    int b=1;
+    cout<<a<<" "<<b<<" ";
+    for(int i=1; i<=x; i++){
+        int nextnum=a+b;
+        cout<<nextnum<<" ";
+        a=b;
+        b=nextnum;
+    }
+   return 0;
+}
