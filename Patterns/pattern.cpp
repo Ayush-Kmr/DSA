29405ce12d3ca9da1872b3fc057fe3c48a3b4729
Ayush <krayushkumar01@gmail.com>
2023-03-27 23:35:09 +0530

    Initial Commit

---
+++ Patterns/pattern.cpp
@@ -0,0 +1,699 @@
+// // Pattern 1
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int x;
+//     cout<<"Enter the number:";
+//     cin>>x;
+//     int i=1;
+//     while(i<=x){
+//         int j=1;
+//         while(j<=x){
+//             cout<<"*";
+//             j=j+1;
+//         }
+//         cout<<endl;
+//         i=i+1;
+//     }
+//    return 0;
+// }
+
+// // Pattern 2
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int x;
+//     cout<<"Enter the number";
+//     cin>>x;
+//     int i=1;
+//     while(i<=x){
+//         int j=1;
+//         while(j<=x){
+//             cout<<i;
+//             j=j+1;
+//         }
+//         cout<<endl;
+//         i=i+1;
+//     }
+//    return 0;
+// }
+
+// // Pattern 3
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int x;
+//     cout<<"Enter the number:";
+//     cin>>x;
+//     int i=1;
+//     while(i<=x){
+//         int j=i;
+//         while(j<=x){
+//             cout<<i;
+//             j=j+1;
+//         }
+//         cout<<endl;
+//         i=i+1;
+//     }
+//    return 0;
+// }
+
+// // Pattern 4
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int x;
+//     cout<<"Enter the number:";
+//     cin>>x;
+//     int i=1;
+//     while(i<=x){
+//         int j=1;
+//         while(j<=x){
+//             cout<<j;
+//             j=j+1;
+//         }
+//         cout<<endl;
+//         i=i+1;
+//     }
+//    return 0;
+// }
+
+// // Pattern 5
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int x;
+//     cout<<"Enter the number:";
+//     cin>>x;
+//     int i=1;
+//     while(i<=x){
+//         int j=1;
+//         // int value=x;
+//         while(j<=x){
+//             cout<<x-j+1;
+//             // cout<<value;
+//             // value=value-1;
+//             j=j+1;
+//         }
+//         cout<<endl;
+//         i=i+1;
+//     }
+//    return 0;
+// }
+
+// // Pattern 6
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int x;
+//     cout<<"Enter the number";
+//     cin>>x;
+//     int i=1;
+//     int count=1;
+//     while(i<=x){
+//         int j=1;
+//         while(j<=x){
+//             cout<<count<<" ";
+//             count=count+1;
+//             j=j+1;
+//         }
+//         cout<<endl;
+//         i=i+1;
+//     }
+//    return 0;
+// }
+
+// // Pattern 7
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int x;
+//     cout<<"Enter the number:";
+//     cin>>x;
+//     int i=1;
+//     while(i<=x){
+//         int j=1;
+//         while(j<=i){
+//             cout<<"*";
+//             j=j+1;
+//         }
+//         cout<<endl;
+//         i=i+1;
+//     }
+//    return 0;
+// }
+
+// // Pattern 8
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int x;
+//     cout<<"Enter the number";
+//     cin>>x;
+//     int i=1;
+//     while(i<=x){
+//         int j=1;
+//         while(j<=i){
+//             cout<<i;
+//             j=j+1;
+//         }
+//         cout<<endl;
+//         i=i+1;
+//     }
+//    return 0;
+// }
+
+// // Pattern 9
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int x;
+//     cout<<"Enter the number";
+//     cin>>x;
+//     int i=1;
+//     int count=1;
+//     while(i<=x){
+//         int j=1;
+//         while(j<=i){
+//             cout<<count<<" ";
+//             count=count+1;
+//             j=j+1;
+//         }
+//         cout<<endl;
+//         i=i+1;
+//     }
+//    return 0;
+// }
+
+// // Pattern 10
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int x;
+//     cout<<"Enter the number:";
+//     cin>>x;
+//     int i=1;
+//     while(i<=x){
+//         int row=i;
+//         int j=1;
+//         while(j<=i){
+//             cout<<row<<" ";
+//             row=row+1;
+//             j=j+1;
+//         }
+//         cout<<endl;
+//         i=i+1;
+//     }
+//    return 0;
+// }
+
+// // Pattern 11
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int x;
+//     cout<<"Enter the number:";
+//     cin>>x;
+//     int i=1;
+//     while(i<=x){
+//         // int row=i;
+//         int j=1;
+//         while(j<=i){
+//             // cout<<row<<" ";
+//             // row=row-1;
+//             cout<<(i-j+1)<<" ";
+//             j=j+1;
+//         }
+//         cout<<endl;
+//         i=i+1;
+//     }
+//    return 0;
+// }
+
+// // Pattern 12
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int x;
+//     cout<<"Enter the number:";
+//     cin>>x;
+//     int i=1;
+//     while(i<=x){
+//         int j=1;
+//         while(j<=x){
+//             char ch='A'+i-1;
+//             cout<<ch;
+//             j=j+1;
+//         }
+//         cout<<endl;
+//         i=i+1;
+//     }
+//    return 0;
+// }
+
+// // Pattern 13
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int x;
+//     cout<<"Enter the number:";
+//     cin>>x;
+//     int i=1;
+//     while(i<=x){
+//         int j=1;
+//         char ch='A';
+//         while(j<=x){
+//             cout<<ch<<" ";
+//             ch=ch+1;
+//             j=j+1;
+//         }
+//         cout<<endl;
+//         i=i+1;
+//     }
+//    return 0;
+// }
+
+// // Pattern 14
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int x;
+//     cout<<"Enter the number:";
+//     cin>>x;
+//     int i=1;
+//     char ch='A';
+//     while(i<=x){
+//         int j=1;
+//         while(j<=x){
+//             cout<<ch<<" ";
+//             ch=ch+1;
+//             j=j+1;
+//         }
+//         cout<<endl;
+//         i=i+1;
+//     }
+//    return 0;
+// }
+
+// // Pattern 15
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int x;
+//     cout<<"Enter the number";
+//     cin>>x;
+//     int i=1;
+//     while(i<=x){
+//         int j=1;
+//         while(j<=x){
+//             char ch='A'+i+j-2;
+//             cout<<ch<<" ";
+//             ch=ch+1;
+//             j=j+1;
+//         }
+//         cout<<endl;
+//         i=i+1;
+//     }
+//    return 0;
+// }
+
+// // Pattern 16
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int x;
+//     cout<<"Enter the number";
+//     cin>>x;
+//     int i=1;
+//     while(i<=x){
+//         int j=1;
+//         char ch='A'+j+i-2;
+//         while(j<=i){
+//             cout<<ch<<" ";
+//             j=j+1;
+//         }
+//         cout<<endl;
+//         i=i+1;
+//     }
+//    return 0;
+// }
+
+// // Pattern 17
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int x;
+//     cout<<"Enter the number";
+//     cin>>x;
+//     int i=1;
+//     char ch='A'+i-1;
+//     while(i<=x){
+//         int j=1;
+//         while(j<=i){
+//             cout<<ch<<" ";
+//             ch=ch+1;
+//             j=j+1;
+//         }
+//         cout<<endl;
+//         i=i+1;
+//     }
+//    return 0;
+// }
+
+// // Pattern 18
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int x;
+//     cout<<"Enter the number:";
+//     cin>>x;
+//     int i=1;
+//     while(i<=x){
+//         int j=1;
+//         while(j<=i){
+//             char ch='A'+i+j-2;
+//             cout<<ch<<" ";
+//             ch=ch+1;
+//             j=j+1;
+//         }
+//         cout<<endl;
+//         i=i+1;
+//     }
+//    return 0;
+// }
+
+// // Pattern 19
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int x;
+//     cout<<"Enter the number:";
+//     cin>>x;
+//     int i=1;
+//     while(i<=x){
+//         int j=1;
+//         char ch='A'+x-i;
+//         while(j<=i){
+//             cout<<ch<<" ";
+//             ch=ch+1;
+//             j=j+1;
+//         }
+//         cout<<endl;
+//         i=i+1;
+//     }
+//    return 0;
+// }
+
+// // Pattern 20
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int x;
+//     cout<<"Enter the number:";
+//     cin>>x;
+//     int i=1;
+//     while(i<=x){
+//         int j=1;
+//         char ch='A'+i-j;
+//         while(j<=x){ 
+//             cout<<ch<<" ";
+//             ch=ch+1;
+//             j=j+1;
+//         }
+//         cout<<endl;
+//         i=i+1;
+//     }
+//    return 0;
+// }
+
+// // Pattern 21
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int x;
+//     cout<<"Enter the number:";
+//     cin>>x;
+//     int i=1;
+//     while(i<=x){
+//         int space=x-i;
+//         while(space){
+//             cout<<" ";
+//             space=space-1;
+//         }
+//         int j=1;
+//         while(j<=i){
+//             cout<<"*";
+//             j=j+1;
+//         }
+//         cout<<endl;
+//         i=i+1;
+//     }
+//    return 0;
+// }
+
+// // Pattern 22
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int x;
+//     cout<<"Enter the number:";
+//     cin>>x;
+//     int i=1;
+//     while(i<=x){
+//         int j=1;
+//         while(j<=(x-i+1)){
+//             cout<<"*";
+//             j=j+1;
+//         }
+//         cout<<endl;
+//         i=i+1;
+//     }
+//    return 0;
+// }
+
+// // Pattern 23
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int x;
+//     cout<<"Enter the number:";
+//     cin>>x;
+//     int i=1;
+//     while(i<=x){
+//         int space=i-1;
+//         while(space){
+//         cout<<" ";
+//         space = space-1;
+//         }
+//         int j=1;
+//         while(j<=(x-i+1)){
+//             cout<<"*";
+//             j=j+1;
+//         }
+//         cout<<endl;
+//         i=i+1;
+//     }
+//    return 0;
+// }
+
+// // Pattern 24
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int x;
+//     cout<<"Enter the number:";
+//     cin>>x;
+//     int i=1;
+//     while(i<=x){
+//         int space=i-1;
+//         while(space){
+//             cout<<" ";
+//             space=space-1;
+//         }
+//         int j=1;
+//         while(j<=(x-i+1)){
+//             cout<<i;
+//             j=j+1;
+//         }
+//         cout<<endl;
+//         i=i+1;
+//     }
+//    return 0;
+// }
+
+// // Pattern 25
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int x;
+//     cout<<"Enter the number:";
+//     cin>>x;
+//     int i=1;
+//     while(i<=x){
+//         int space =x-i;
+//         while(space){
+//             cout<<" ";
+//             space=space-1;
+//         }
+//         int j=1;
+//         while(j<=i){
+//             cout<<i;
+//             j=j+1;
+//         }
+//         cout<<endl;
+//         i=i+1;
+//     }
+//    return 0;
+// }
+
+// // Pattern 26
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int x;
+//     cout<<"Enter the number:";
+//     cin>>x;
+//     int i=1;
+//     while(i<=x){
+//         int space=i-1;
+//         while(space){
+//             cout<<" ";
+//             space =space-1;
+//         }
+//         int j=1;
+//         int row=i;
+//         while(j<=(x-i+1)){
+//             cout<<row;
+//             row=row+1;
+//             j=j+1;
+//         }
+//         cout<<endl;
+//         i=i+1;
+//     }
+//    return 0;
+// }
+
+// // Pattern 27
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int x;
+//     cout<<"Enter the number:";
+//     cin>>x;
+//     int i=1;
+//     int row=i;
+//     while(i<=x){
+//         int space=x-i;
+//         while(space){
+//             cout<<" ";
+//             space=space-1;
+//         }
+//         int j=1;
+//         while(j<=i){
+//             cout<<row;
+//             row=row+1;
+//             j=j+1;
+//         }
+//         cout<<endl;
+//         i=i+1;
+//     }
+//    return 0;
+// }
+
+// // Pattern 28
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int x;
+//     cout<<"Enter the number:";
+//     cin>>x;
+//     int i=1;
+//     while(i<=x){
+//         int space=x-i;
+//         while(space){
+//             cout<<" ";
+//             space=space-1;
+//         }
+//         int j=1;
+//         while(j<=i){
+//             cout<<j;
+//             j=j+1;
+//         }
+//         int row=i;
+//         int start=row-1;
+//         while(start){
+//             row=row-1;
+//             cout<<row;
+//             start=start-1;
+//         }
+//         cout<<endl;
+//         i=i+1;
+//     }
+//    return 0;
+// }
+
+// // Pattern 29
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int x;
+//     cout<<"Enter the number:";
+//     cin>>x;
+//     int i=1;
+//     while(i<=x){
+//         int j=1;
+//         while(j<=(x-i+1)){
+//             cout<<j;
+//             j=j+1;
+//         }
+//         // For Stars
+//         int star1=i-1;
+//         while(star1){
+//             cout<<"*";
+//             star1=star1-1;
+//         }
+//         int star2=i-1;
+//         while(star2){
+//             cout<<"*";
+//             star2=star2-1;
+//         }
+//         // For numeric value after stars
+//         int value=x-i+1;
+//         while(value){
+//             cout<<value;
+//             value=value-1;
+//         }
+//         cout<<endl;
+//         i=i+1;
+//     }
+//    return 0;
+// }
