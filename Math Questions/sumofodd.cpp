9ca687e472796ac9f6c40aa7848072e5234005de
Ayush <krayushkumar01@gmail.com>
2023-03-31 22:08:25 +0530

    Initial Commit

---
+++ Math Questions/sumofodd.cpp
@@ -0,0 +1,19 @@
+// Program to print sum of all odd number in the given range.
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter the number:";
+    cin>>n;
+    int i=1;
+    int sum=0;
+    while(i<=n){
+        if(i%2!=0){
+        sum=sum+i;
+        }
+        i=i+1;
+    }
+    cout<<sum<<endl;
+   return 0;
+}
