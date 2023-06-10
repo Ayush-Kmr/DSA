ee49ca5bd1947a520ceaab731ab274c01fae13d1
Ayush <krayushkumar01@gmail.com>
2023-06-10 15:06:18 +0530

    Initial Commit

--- Math Questions/factorialofnum.cpp
+++ Math Questions/factorialofnum.cpp
@@ -1,3 +1,4 @@
+// Iterative Implementation
 #include<bits/stdc++.h>
 using namespace std;
 int fact(int x){
@@ -14,5 +15,28 @@ int main()
     cin>>x;
     fact(x);
     cout<<"Factorial of a given number "<<x<<" is "<<fact(x)<<endl;
+   return 0;
+}
+
+
+
+// Recursive Factorial Function
+#include<bits/stdc++.h>
+using namespace std;
+int fact(int x){
+    if(x==0){
+        return 1;
+    }
+    else {
+        return x*fact(x-1);
+    }
+}
+int main()
+{
+    int x;
+    cout<<"Enter the number:";
+    cin>>x;
+    fact(x);
+    cout<<"Factorial of a given number "<< x <<" is "<<fact(x);
    return 0;
 }
--- Math Questions/tempCodeRunnerFile.cpp
+++ Math Questions/tempCodeRunnerFile.cpp
@@ -1,27 +1,19 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
+int fact(int x){
+    if(x==0){
+        return 1
+    }
+    else {
+        return x*fact(x-1);
+    }
+}
 int main()
 {
     int x;
-    cout << "Enter the number:";
-    cin >> x;
-    int c = 0;
-    for (int i = 2; i < x; i++)
-    {
-        if (x % i == 0)
-        {
-            c++;
-            break;
-        }
-    }
-    if (c > 0)
-    {
-        cout << "Number is not prime" << endl;
-    }
-    else
-    {
-        cout << "Number is prime" << endl;
-    }
-
-    return 0;
+    cout<"Enter the number:";
+    cin>>x;
+    fact(x);
+    cout<<"Factorial of a given number "<< x <<" is"<<fact(x);
+   return 0;
 }
