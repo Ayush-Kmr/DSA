acff038a55ff1c9f170aca23b160aa3544e24c63
Ayush <krayushkumar01@gmail.com>
2023-08-07 01:28:16 +0530

    Initial Commit

--- DSA(Questions)/Array/binarysearch.cpp
+++ DSA(Questions)/Array/binarysearch.cpp
@@ -6,7 +6,8 @@ int binarySearch(int arr[], int size, int key)
     int start = 0;
     int end = size - 1;
 
-    int mid = (start + end) / 2;
+    // int mid = (start + end) / 2;
+    int mid = start + (end - start) / 2;
 
     while (start <= end)
     {
@@ -24,7 +25,8 @@ int binarySearch(int arr[], int size, int key)
         {
             end = mid - 1;
         }
-        mid = (start + end) / 2;
+        // mid = (start + end) / 2;
+        int mid = start + (end - start) / 2;
     }
     return -1;
 }
